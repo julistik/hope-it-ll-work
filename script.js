a0003eab802c0b5b8d4620f85b77051234bb2cd0
Julia <79375782+julistik@users.noreply.github.com>
2024-11-06 10:06:07 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+function afterr(){
+    document.getElementById('town')
+    .innerText="UMVD ROSSII PO G. TULA";
+    document.getElementById('surname')
+    .innerText="VOROBEVA";
+    document.getElementById('name')
+    .innerText="YULIA";
+    document.getElementById('father_name')
+    .innerText="ANATOLEVNA";
+    document.getElementById('sex')
+    .innerText="F";
+    document.getElementById('birthday_place')
+    .innerText="G. TULA";
+}
+
+function beforre(){
+    document.getElementById('town')
+    .innerText="УМВД РОССИИ ПО Г. ТУЛА";
+    document.getElementById('surname')
+    .innerText="ВОРОБЬЁВА";
+    document.getElementById('name')
+    .innerText="ЮЛИЯ";
+    document.getElementById('father_name')
+    .innerText="АНАТОЛЬЕВНА";
+    document.getElementById('sex')
+    .innerText="Ж";
+    document.getElementById('birthday_place')
+    .innerText="Г. ТУЛА";
+}
